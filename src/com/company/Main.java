1c4f2a75b5bdb8e587f8519d71ff4a281d1238f3
Konrad Żochowski <zochowski.konrad@gmail.com>
2018-07-06 13:03:46 +0200

    Application frame created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+
+    public Main(){
+        super("Zipper");
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int appHeight = screenHeight/3;
+        int appWidth = screenWidth/3;
+
+        this.setBounds((screenWidth-appWidth)/2,(screenHeight-appHeight)/2,appWidth,appHeight);
+
+
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
     public static void main(String[] args) {
-	// write your code here
+        new Main().setVisible(true);
     }
 }
